e2143c97b85600795aa26a67f90b4c0c4d93c7f2
Alex Crichton <alex.crichton@onemodel.co>
2017-04-05 11:54:37 +1000

    Initial

---
+++ AppveyorCsharp7/AppveyorCsharp7/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace AppveyorCsharp7
+{
+    public class Class1
+    {
+        public void Test()
+        {
+            var parent = new Parent();
+            if (parent is Child child)
+            {
+
+            }
+        }
+    }
+
+    class Parent { }
+
+    class Child : Parent { }
+}
